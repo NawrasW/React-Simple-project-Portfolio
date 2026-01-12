33ef9068fe3c8f3c69dbaf5a79d69a863a655c0b
NawrasW <0987654321plmplmplm@gmail.com>
2026-01-12 15:16:59 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://nawrasw.github.io/React-Simple-project-Portfolio/", 
+  base: "/React-Simple-project-Portfolio/", 
   plugins: [react()],
 })
